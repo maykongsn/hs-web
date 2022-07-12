66f059c68dd75689b34c1b241d83196eafe3ed32
maykongsn <contatomaykon@hotmail.com>
2022-07-11 23:05:23 -0300

    feat: Criação do login para técnico

--- src/pages/LoginTechnician/index.jsx
+++ src/pages/LoginTechnician/index.jsx
@@ -23,26 +23,24 @@ export function LoginTechnician() {
     } else {
         return (
             <div className="login-container">
-                <div>
-                    <section className="form">
-                        <form onSubmit={handleSignIn}>
-                            <h1>Faça seu Login</h1>
-                            <input
-                                type="text"
-                                placeholder="Digite seu e-mail"
-                                value={email ?? ""}
-                                name="email"
-                                onChange={(event) => { setEmail(event.target.value) }} />
-                            <input
-                                type="password"
-                                placeholder="Digite sua senha"
-                                value={password ?? ""}
-                                name="password"
-                                onChange={(event) => { setPassword(event.target.value) }} />
-                            <button className="button" type="submit">Entrar</button>
-                        </form>
-                    </section>
-                </div>
+                <section className="form">
+                    <form onSubmit={handleSignIn}>
+                        <h1>Faça seu Login</h1>
+                        <input
+                            type="text"
+                            placeholder="Digite seu e-mail"
+                            value={email ?? ""}
+                            name="email"
+                            onChange={(event) => { setEmail(event.target.value) }} />
+                        <input
+                            type="password"
+                            placeholder="Digite sua senha"
+                            value={password ?? ""}
+                            name="password"
+                            onChange={(event) => { setPassword(event.target.value) }} />
+                        <button className="button" type="submit">Entrar</button>
+                    </form>
+                </section>
             </div>
         );
     }
